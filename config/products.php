f2dd09d0d33b1d9e1e846fa1539253db264c6505
andrej <andrik200010@gmail.com>
2022-03-26 01:33:44 +0300

    add config file

---
+++ config/products.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+     * The role of the user who can edit the data
+     */
+    'role' => 'admin',
+
+
+    /*
+     * The address to which the notification is sent
+     */
+    'email' => 'testovoe@zadanie.ru',
+
+];
