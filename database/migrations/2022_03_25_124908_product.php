5c6027695dc979cfbd604eaa96af8f8c81fe5eb8
andrej <andrik200010@gmail.com>
2022-03-25 16:50:11 +0300

    create db

---
+++ database/migrations/2022_03_25_124908_product.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('ID');
+            $table->string('ARTICLE', 255)->unique()->index();
+            $table->string('NAME', 255);
+            $table->jsonb('DATA');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
